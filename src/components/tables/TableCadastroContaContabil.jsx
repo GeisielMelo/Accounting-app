1f71f514ecb516d14c42e6a9b5b9a967ad82a8e7
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-09 16:18:01 -0300

    refactor: Applied changes after refactoring the components.

--- src/components/tables/TableCadastroContaContabil.jsx
+++ src/components/tables/TableCadastroContaContabil.jsx
@@ -1,42 +1,13 @@
 import React, { useMemo } from "react";
-import styled from "styled-components";
+import { TableContainer, TableNav } from "../../styles/StyledTable";
 
 import { useTable, usePagination, useRowSelect, useGlobalFilter, useSortBy } from "react-table";
 import MOCK_DATA from "../../config/MOCK_DATA.json";
 import { COLUMNS } from "../../utils/tableCadastroContaContabil";
 import { Checkbox } from "../Checkbox";
+import { Pagination } from "../Pagination";
 import { GlobalFilter } from "../GlobalFilter";
-
-const TableContainer = styled.div`
-  table {
-    font-family: Arial, Helvetica, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-  }
-
-  table td,
-  table th {
-    border: 1px solid #ddd;
-    padding: 8px;
-    text-align: center;
-  }
-
-  table tr:nth-child(even) {
-    background-color: #f2f2f2;
-  }
-
-  table tr:hover {
-    background-color: #ddd;
-  }
-
-  table th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: center;
-    background-color: #04aa6d;
-    color: white;
-  }
-`;
+import { TableFooter } from "../TableFooter";
 
 export const TableCadastroContaContabil = () => {
   const columns = useMemo(() => COLUMNS, []);
@@ -54,12 +25,12 @@ export const TableCadastroContaContabil = () => {
     (hooks) => {
       hooks.visibleColumns.push((columns) => {
         return [
+          ...columns,
           {
             id: "selection",
             Header: ({ getToggleAllRowsSelectedProps }) => <Checkbox {...getToggleAllRowsSelectedProps()} />,
             Cell: ({ row }) => <Checkbox {...row.getToggleRowSelectedProps()} />,
           },
-          ...columns,
         ];
       });
     }
@@ -89,7 +60,11 @@ export const TableCadastroContaContabil = () => {
 
   return (
     <>
-      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <TableNav>
+        <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+        <Pagination onChange={(e) => setPageSize(Number(e.target.value))} pageSize={pageSize} />
+      </TableNav>
+
       <TableContainer>
         <table {...getTableProps()}>
           <thead>
@@ -117,56 +92,20 @@ export const TableCadastroContaContabil = () => {
         </table>
       </TableContainer>
 
-      <div>
-        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-          {"<<"}
-        </button>
-        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-          {"<"}
-        </button>
-
-        <span>
-          Page {pageIndex + 1} of {pageOptions.length}{" "}
-        </span>
-
-        <span>
-          Go to page:{" "}
-          <input
-            type="number"
-            placeholder=""
-            defaultValue={pageIndex + 1}
-            onChange={(e) => {
-              const page = e.target.value ? Number(e.target.value) - 1 : 0;
-              gotoPage(page);
-            }}
-          />
-        </span>
-
-        <button onClick={() => nextPage()} disabled={!canNextPage}>
-          {">"}
-        </button>
-        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-          {">>"}
-        </button>
-
-        <select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
-          {[10, 20, 30, 40, 50].map((pageSize) => (
-            <option key={pageSize} value={pageSize}>
-              Show {pageSize}
-            </option>
-          ))}
-        </select>
-      </div>
-
-      {/* <code>
-        {JSON.stringify(
-          {
-            selectedFlatRows: selectedFlatRows.map((row) => row.original),
-          },
-          null,
-          2
-        )}
-      </code> */}
+      <TableFooter
+        nextBtn={() => nextPage()}
+        prevBtn={() => previousPage()}
+        lastPage={() => gotoPage(pageCount - 1)}
+        firstPage={() => gotoPage(0)}
+        nextDisabled={!canNextPage}
+        prevDisabled={!canPreviousPage}
+        pageIndex={pageIndex + 1}
+        pageLength={pageOptions.length}
+        onChange={(e) => {
+          const page = e.target.value ? Number(e.target.value) - 1 : 0;
+          gotoPage(page);
+        }}
+      />
     </>
   );
 };
