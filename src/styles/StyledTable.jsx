ab60563fedeefb0c0a7045e73d02fc47229a5cb9
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-09 16:22:11 -0300

    style: Added a new styled component for the main table CSS.

---
+++ src/styles/StyledTable.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const TableContainer = styled.div`
+  overflow-x: auto;
+  table {
+    font-family: Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+  }
+
+  table td,
+  table th {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: center;
+  }
+
+  table tr:nth-child(even) {
+    background-color: #f2f2f2;
+  }
+
+  table tr:hover {
+    background-color: #ddd;
+  }
+
+  table th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: center;
+    background-color: #04aa6d;
+    color: white;
+  }
+`;
+
+export const TableNav = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 10px 0;
+  gap: 10px;
+`;
