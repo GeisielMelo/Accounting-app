f43513439de890094e268564c308ceb02e7b53cc
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-09 23:34:58 -0300

    test: testing the new FileUpload component

--- src/pages/navigation/QuebrarArquivo.jsx
+++ src/pages/navigation/QuebrarArquivo.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { FileUpload } from "../../components/FileUpload";
 
 const QuebrarArquivo = () => {
-  return <p>QuebrarArquivo</p>;
+  const [jsonArray, setJsonArray] = useState([]);
+
+  return (
+    <div>
+      <FileUpload setJsonArray={setJsonArray} />
+      <button onClick={() => console.log(jsonArray[0])}>Log - Primeiro Item</button>
+    </div>
+  );
 };
 
 export default QuebrarArquivo;
